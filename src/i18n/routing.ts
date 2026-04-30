cb3f7f64c855aed9609307a5cb8926403dcfdb39
Andre Almeida <andrealmeida@Andres-MacBook-Air.local>
2026-04-30 20:28:25 -0300

    feat: add Spanish (es) locale with full translations

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,7 +2,7 @@ import { defineRouting } from "next-intl/routing"
 import { createNavigation } from "next-intl/navigation"
 
 export const routing = defineRouting({
-  locales: ["en", "pt-BR"],
+  locales: ["en", "pt-BR", "es"],
   defaultLocale: "en",
   localePrefix: "as-needed",
 })
