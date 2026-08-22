f29d4166c7d178424b547470b14e1d3103bc613d
andreajoa <100246160+andreajoa@users.noreply.github.com>
2026-08-21 23:31:24 -0300

    feat: switch homepage to premium redesign

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,25 +1,5 @@
-import { HeroSection } from "@/components/home/HeroSection"
-import { PainPointsSection } from "@/components/home/PainPointsSection"
-import { ServicesOverview } from "@/components/home/ServicesOverview"
-import { PortfolioHighlights } from "@/components/home/PortfolioHighlights"
-import { LatestRealProjects } from "@/components/home/LatestRealProjects"
-import { Testimonials } from "@/components/home/Testimonials"
-import { ProcessSection } from "@/components/home/ProcessSection"
-import { FAQSection } from "@/components/home/FAQSection"
-import { CTASection } from "@/components/home/CTASection"
+import { PremiumHome } from "@/components/home/PremiumHome"
 
 export default function HomePage() {
-  return (
-    <>
-      <HeroSection />
-      <PainPointsSection />
-      <ServicesOverview />
-      <PortfolioHighlights />
-      <LatestRealProjects />
-      <Testimonials />
-      <ProcessSection />
-      <FAQSection />
-      <CTASection />
-    </>
-  )
+  return <PremiumHome />
 }
